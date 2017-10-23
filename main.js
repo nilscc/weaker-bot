04a2d77cf3ecdcdd759368826a3de13ee97e12d0
Chris Ernst <cernst3@gatech.edu>
2017-10-23 17:38:46 -0400

    8 hour sotd

--- main.js
+++ main.js
@@ -165,20 +165,20 @@ bot.addListener("message", function(from, to, text, message) {
 	}
 	if(splitup[0].toLowerCase() == "..sotd" || splitup[0].toLowerCase() == "sotd"){
 		if(splitup[1] == undefined){
-			if((sotd.time+(60.0*60.0*24.0)) < (Date.now()/1000.0)){
+			if((sotd.time+(60.0*60.0*8.0)) < (Date.now()/1000.0)){
 				bot.say(config.channels[0], "time for a new song. link pls");
 			}
 			else{
 				bot.say(config.channels[0], sotd.who+"'s song of the day for "+timeDifference(sotd.time)+" longer is "+sotd.link);
 			}
 		}
-		else if((sotd.time+(60.0*60.0*24.0)) < (Date.now()/1000.0)){
+		else if((sotd.time+(60.0*60.0*8.0)) < (Date.now()/1000.0)){
 			if(splitup[1].indexOf("http") < 0) bot.say(config.channels[0], from+" "+randomFromArray(rebuke));
 			else{
 				sotd.link = splitup[1];//sanatize this better?
 				sotd.time = Date.now()/1000.0;
 				sotd.who = from;
-				bot.say(config.channels[0], "OK you got the song of the day for 24 hours");
+				bot.say(config.channels[0], "OK you got the song of the day for 8 hours");
 			}
 		}
 		else{
@@ -288,7 +288,7 @@ function localTime(who, when){
 }
 
 function timeDifference(time){
-	var msec = ((sotd.time+(60.0*60.0*24.0))*1000.0) - Date.now();
+	var msec = ((sotd.time+(60.0*60.0*8.0))*1000.0) - Date.now();
 	var hours = Math.floor(msec / 1000 / 60 / 60);
 	msec -= hours * 1000 * 60 * 60;
 	var mins = Math.floor(msec / 1000 / 60);
