399dc5bcd686c152f7fa744abf0465f56ab49e1b
Laura <cmgraves@mun.ca>
2019-05-30 18:09:34 -0230

    global replace

--- main.js
+++ main.js
@@ -569,8 +569,7 @@ function dirtyWords(wordsToDefine) {
 			var definition = sample.list[0];
 			if (definition != undefined) {
 				var definition = sample.list[0].definition;
-				definition = definition.replace("[", "");
-				definition = definition.replace("]", "");
+				definition = definition.replace(/[\[\]]/g, "");
 				var defined = wordsToDefine.join(" ") + ": " + definition;
 				bot.say(config.channels[0], defined);
 			} else {
