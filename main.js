f5d2045572aaeb2eaf3c33b9ea1c29dd3b702249
nils.cc <mail@nils.cc>
2022-03-15 00:11:15 +0100

    Move ..lift and ..tell to separate modules

--- main.js
+++ main.js
@@ -75,16 +75,14 @@ const noLifts = [
 	"guilt", "shame", "baggage", "error 404", "nothing", "nada", "zip", "zilch"];
 const hiNombski = ["too many cooks", "how's the rash?", "nvm I'll tell you later, he's here"];
 
-const tells = {};
 const sotd = {
 	"time": 0,
 	"link": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 	"who": "yo momma"
 };
 
+import * as tell from "./commands/tell.js";
 import irc from "irc";
-import { MongoClient } from 'mongodb';
-
 import fetch from 'node-fetch';
 
 async function request(url, cb) {
@@ -100,7 +98,6 @@ async function request(url, cb) {
     cb(err, resp, body);
 }
 
-config.url = "mongodb://127.0.0.1:27017/weakdb";
 const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
 
 // var bot = new irc.Client("irc.libera.chat", "weakerbot", {
@@ -196,7 +193,8 @@ bot.addListener("message", function (from, to, text, message) {
 	}
 
 	if (splitup[0].toLowerCase() == "..help" || splitup[0].toLowerCase() == "!help" || splitup[0].toLowerCase() == ".help") {
-		bot.say(config.channels[0], "..ping, ..sing, ..insult [name], ..dab [name], ..tell nick message, ..sotd [link], ..lifts [who] [lift] [[weight units reps]], ..$ currency, ..ud word, ..weather city/code, country code");
+        // TODO: ..lifts [who] [lift] [[weight units reps]]
+		bot.say(config.channels[0], "..ping, ..sing, ..insult [name], ..dab [name], ..tell nick message, ..sotd [link], ..$ currency, ..ud word, ..weather city/code, country code");
 		return;
 	}
 
@@ -265,146 +263,12 @@ bot.addListener("message", function (from, to, text, message) {
 		}
 		return;
 	}
-	if (splitup[0].toLowerCase().indexOf("..lifts") > -1 || splitup[0].toLowerCase().indexOf("..lift") > -1 || splitup[0].toLowerCase().indexOf("!lifts") > -1 || splitup[0].toLowerCase().indexOf(".lifts") > -1) {
-		//..lifts
-		//..lifts bench
-		//..lifts trefirefem
-		//..lifts trefirefem bench
-		//..lifts bench 100 kg 2
 
-		if (!splitup[1]) {
-			//get all the lifts for who sent the message
-			//..lifts
-			console.log("..lifts");
-			var weight = {"squat": "guilt", "bench": "shame", "deadlift": "emotional baggage", "ohp": "depression"};
-			var units = {"squat": "", "bench": "", "deadlift": "", "ohp": ""};
-			var reps = {"squat": 0, "bench": 0, "deadlift": 0, "ohp": 0};
-			MongoClient.connect(config.url, function (err, db) {
-				if (err) throw err;
-				db.collection("lifts").find({"who": from.toLowerCase()}).toArray(function (err, res) {
-					if (err) throw err;
-					// console.log(res);
-					for (var i = res.length - 1; i >= 0; i--) {
-						weight[res[i].lift] = res[i].weight;
-						units[res[i].lift] = res[i].unit;
-						reps[res[i].lift] = res[i].reps;
-					}
-					db.close();
-					if (!res.length) bot.say(config.channels[0], from + " add some lifts first");
-					var squatstring = reps.squat > 1 ? "x" + reps.squat : "";
-					squatstring = weight.squat + units.squat + squatstring;
-					var benchstring = reps.bench > 1 ? "x" + reps.bench : "";
-					benchstring = weight.bench + units.bench + benchstring;
-					var deadstring = reps.deadlift > 1 ? "x" + reps.deadlift : "";
-					deadstring = weight.deadlift + units.deadlift + deadstring;
-					var ohpstring = reps.ohp > 1 ? "x" + reps.ohp : "";
-					ohpstring = weight.ohp + units.ohp + ohpstring;
-					var string = from + " " + squatstring + "/" + benchstring + "/" + deadstring + "/" + ohpstring;
-					var e1rmstring = " (e1RMs: " + epley(weight.squat, reps.squat) + units.squat + "/" + epley(weight.bench, reps.bench) + units.bench + "/" + epley(weight.deadlift, reps.deadlift) + units.deadlift + "/" + epley(weight.ohp, reps.ohp) + units.ohp + ")";
-					if (res.length) bot.say(config.channels[0], string + "" + e1rmstring);
-				});
-			});
-		} else if (!splitup[2] && (splitup[1].toLowerCase() != "squat" && splitup[1].toLowerCase() != "bench" && splitup[1].toLowerCase() != "deadlift" && splitup[1].toLowerCase() != "ohp")) {
-			//get the lift for splitup[1]
-			//..lifts trefirefem
-			console.log("..lifts trefirefem");
-			var weight = {"squat": "guilt", "bench": "shame", "deadlift": "emotional baggage", "ohp": "depression"};
-			var units = {"squat": "", "bench": "", "deadlift": "", "ohp": ""};
-			var reps = {"squat": 0, "bench": 0, "deadlift": 0, "ohp": 0};
-			MongoClient.connect(config.url, function (err, db) {
-				if (err) throw err;
-				db.collection("lifts").find({"who": splitup[1].toLowerCase()}).toArray(function (err, res) {
-					if (err) throw err;
-					// console.log(res);
-					for (var i = res.length - 1; i >= 0; i--) {
-						weight[res[i].lift] = res[i].weight;
-						units[res[i].lift] = res[i].unit;
-						reps[res[i].lift] = res[i].reps;
-					}
-					db.close();
-					if (!res.length) bot.say(config.channels[0], from + " couldn't find lifts for " + splitup[1] + ". Is that supposed to be a person or a lift? E.g. ..lifts trefirefem for all of trefirefem's lifts, or ..lift bench for your bench numbers");
-					// var string = from+" "+splitup[1]+" squat: "+weight.squat+""+units.squat+" for "+reps.squat+" (e1rm "+epley(weight.squat, reps.squat)+""+units.squat+")";
-					// string += " bench: "+weight.bench+""+units.bench+" for "+reps.bench+" (e1rm "+epley(weight.bench, reps.bench)+""+units.bench+")";
-					// string += " deadlift: "+weight.deadlift+""+units.deadlift+" for "+reps.deadlift+" (e1rm "+epley(weight.deadlift, reps.deadlift)+""+units.deadlift+")";
-					// string += " ohp: "+weight.ohp+""+units.ohp+" for "+reps.ohp+" (e1rm "+epley(weight.ohp, reps.ohp)+""+units.ohp+")";
-					var squatstring = reps.squat > 1 ? "x" + reps.squat : "";
-					squatstring = weight.squat + units.squat + squatstring;
-					var benchstring = reps.bench > 1 ? "x" + reps.bench : "";
-					benchstring = weight.bench + units.bench + benchstring;
-					var deadstring = reps.deadlift > 1 ? "x" + reps.deadlift : "";
-					deadstring = weight.deadlift + units.deadlift + deadstring;
-					var ohpstring = reps.ohp > 1 ? "x" + reps.ohp : "";
-					ohpstring = weight.ohp + units.ohp + ohpstring;
-					var string = from + " " + squatstring + "/" + benchstring + "/" + deadstring + "/" + ohpstring;
-					var e1rmstring = " (e1RMs: " + epley(weight.squat, reps.squat) + units.squat + "/" + epley(weight.bench, reps.bench) + units.bench + "/" + epley(weight.deadlift, reps.deadlift) + units.deadlift + "/" + epley(weight.ohp, reps.ohp) + units.ohp + ")";
-					if (res.length) bot.say(config.channels[0], string + "" + e1rmstring);
-				});
-			});
-		} else if (!splitup[2] && (splitup[1].toLowerCase() == "squat" || splitup[1].toLowerCase() == "bench" || splitup[1].toLowerCase() == "deadlift" || splitup[1].toLowerCase() == "ohp")) {
-			//get the lift for the person who sent the message
-			//..lifts bench
-			console.log("..lifts bench");
-			MongoClient.connect(config.url, function (err, db) {
-				if (err) throw err;
-				db.collection("lifts").findOne({
-					"who": from.toLowerCase(),
-					"lift": splitup[1].toLowerCase()
-				}, function (err, res) {
-					if (err) throw err;
-					// console.log(res[0]);
-					db.close();
-					if (res == null) bot.say(config.channels[0], from + " uh hmm didn't find that... Is that lift added? E.g. ..lifts bench");
-					if (res != null) bot.say(config.channels[0], from + " you " + splitup[1] + " " + res.weight + "" + res.unit + " for " + res.reps + " (e1rm " + epley(res.weight, res.reps) + "" + res.unit + ")");
-				});
-			});
-		} else if (splitup[2].toLowerCase() == "squat" || splitup[2].toLowerCase() == "bench" || splitup[2].toLowerCase() == "deadlift" || splitup[2].toLowerCase() == "ohp") {
-			//get the lifts for splitup[1]
-			//..lifts trefirefem bench
-			console.log("..lifts trefirefem bench");
-			MongoClient.connect(config.url, function (err, db) {
-				if (err) throw err;
-				db.collection("lifts").findOne({
-					"who": splitup[1].toLowerCase(),
-					"lift": splitup[2].toLowerCase()
-				}, function (err, res) {
-					if (err) throw err;
-					// console.log(res[0]);
-					db.close();
-					if (res == null) bot.say(config.channels[0], from + " uh hmm didn't find that lift for person. E.g. ..lifts trefirefem bench");
-					if (res != null) bot.say(config.channels[0], from + " " + splitup[1] + " " + splitup[2] + " " + res.weight + "" + res.unit + " for " + res.reps + " (e1rm " + epley(res.weight, res.reps) + "" + res.unit + ")");
-				});
-			});
-		} else if (splitup[2].toLowerCase() != "squat" && splitup[2].toLowerCase() != "bench" && splitup[2].toLowerCase() != "deadlift" && splitup[2].toLowerCase() != "ohp") {
-			//update the lift for the person who sent the message
-			//..lifts bench 100 kg 2
-			console.log("..lifts bench 100 kg 2");
-			if (isNaN(splitup[2] || isNaN(splitup[4]))) {
-				bot.say(config.channels[0], from + " ..lifts [squat/bench/deadlift/ohp] [weight] [units] [reps] (e.g. ..lifts bench 100 lbs 2)");
-				return;
-			}
-			MongoClient.connect(config.url, function (err, db) {
-				if (err) throw err;
-				db.collection("lifts").update({
-					"who": from.toLowerCase(),
-					"lift": splitup[1].toLowerCase()
-				}, {
-					"who": from.toLowerCase(),
-					"lift": splitup[1].toLowerCase(),
-					"weight": splitup[2],
-					"unit": splitup[3].toLowerCase(),
-					"reps": splitup[4]
-				}, {upsert: true}, function (err, res) {
-					if (err) throw err;
-					// console.log("did find");
-					// console.log(res);
-					db.close();
-					if (res == null) bot.say(config.channels[0], from + " oh, uh hmm didn't find that...?");
-					if (res != null) bot.say(config.channels[0], from + " gj bb! That's an e1rm of " + epley(splitup[2], splitup[4]) + "" + splitup[3]);
-				});
-			});
-		} else bot.say(config.channels[0], from + " ..lifts [who] [squat/bench/deadlift/ohp] ([weight] [units] [reps]) (e.g. ..lift trefirefem or ..lifts bench 100 lbs 2)");
-	}
-	unreadMessages(from);
+    // TODO: ..lift
+
+    // check unread ..tell messages
+	tell.checkUnread(from);
+
 	if (splitup[0].toLowerCase() == "..tell" || splitup[0].toLowerCase() == "!tell" || splitup[0].toLowerCase() == ".tell") {
 		if (splitup[1] && splitup[1].toLowerCase().indexOf("bot") > -1 && splitup[1].toLowerCase().indexOf("ferboten") < 0) {
 			bot.say(config.channels[0], randomFromArray(rebuke));
@@ -417,32 +281,12 @@ bot.addListener("message", function (from, to, text, message) {
 			var msg = splitup.slice(2); //sanitize this too?
 			//accept and escape special chars
 			msg = msg.join(" ");
-			var tell = {
-				"from": from,
-				"to": to,
-				"time": Date.now() / 1000.0,
-				"msg": msg
-			};
-			//store it
-			// if(tells[to]){
-			// 	tells[to].push(tell);
-			// }
-			// else{
-			// 	tells[to] = [tell];
-			// }
-			// bot.say(config.channels[0], tell.from+", OK I'll try, but don't count on it. This is beta af");
-			MongoClient.connect(config.url, function (err, db) {
-				if (err) throw err;
-				db.collection("tells").insertOne(tell, function (err, res) {
-					if (err) throw err;
-					console.log("stored tell");
-					console.log(tell);
-					db.close();
-					bot.say(config.channels[0], tell.from + ", OK got it");
-				});
-			});
+
+            // store message in database
+            tell.create(from, to, msg);
 		}
 	}
+
 	if (splitup[0].toLowerCase() == "..weather" || splitup[0].toLowerCase() == "!weather" || splitup[0].toLowerCase() == ".weather" || splitup[0].toLowerCase() == "..w" || splitup[0].toLowerCase() == "!w" || splitup[0].toLowerCase() == ".w") {
 		//..weather london
 		//weather london, uk
@@ -458,6 +302,7 @@ bot.addListener("message", function (from, to, text, message) {
 			getWeather(query);
 		} else bot.say(config.channels[0], "But where tho");
 	}
+
 	if (splitup[0].toLowerCase() == "..$" || splitup[0].toLowerCase() == "!$" || splitup[0].toLowerCase() == ".$") {
 		//..$ 11 nok
 		//.$ 123232423423423 CAD
@@ -483,37 +328,6 @@ bot.addListener("message", function (from, to, text, message) {
 
 function randomFromArray(array) {
 	return array[Math.floor(Math.random() * array.length)];
-}
-
-function unreadMessages(from) {
-	var oldFrom = from;
-	from = from.toLowerCase();
-	// console.log("check msg for "+from);
-	MongoClient.connect(config.url, function (err, db) {
-		if (err) throw err;
-		db.collection("tells").findOne({"to": from}, function (err, res) {
-			if (err) throw err;
-			db.close();
-			if (res !== null && res !== undefined) {
-				console.log("unread tell");
-				console.log(res);
-				bot.say(config.channels[0], from + " message from " + res.from + "" + localTime(res.to, res.time) + ": " + res.msg);
-				deleteTell(res);
-			}
-		});
-	});
-}
-
-function deleteTell(tell) {
-	MongoClient.connect(config.url, function (err, db) {
-		if (err) throw err;
-		db.collection("tells").deleteOne({"_id": tell._id}, function (err, res) {
-			if (err) throw err;
-			console.log("deleted tell");
-			console.log(tell);
-			db.close();
-		});
-	});
 }
 
 function localTime(who, when) {
