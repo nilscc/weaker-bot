2012ccde5c93bd57e3b88aa4675a00cc0a06fb91
nils.cc <mail@nils.cc>
2022-03-15 20:20:09 +0100

    remove nickserv password

--- config.example.js
+++ config.example.js
@@ -5,6 +5,6 @@ export default {
     channels: ["##weakpots"],
     server: "irc.libera.chat",
     botName: "weakerbot",
-    password: "weakness420",
+    password: "", // your nickserv password
     openWeatherToken: "" // get from https://home.openweathermap.org/api_keys
 };
